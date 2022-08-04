078f65bed9c833cb113c67b0c54e1cc536a7593a
Aaron Kromer <kromer.aaron@gmail.com>
2022-08-04 17:52:03 +0200

    remove unncecessarry import

--- src/lol/processors/gameStart.ts
+++ src/lol/processors/gameStart.ts
@@ -1,6 +1,5 @@
 import { GameStartEvent } from "../../model";
 import { getVoiceLineWithTags, playVoiceLine } from "../../utils";
-import { VoiceLineTag } from "../../voiceLineConfig";
 
 export const GameStart = async ({ EventName }: GameStartEvent) => {
   await playVoiceLine(getVoiceLineWithTags(EventName));
